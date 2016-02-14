bf57f691579451bcdb6d4e758d7fd1e96d84b087
TiciaChance <laticia.chance@gmail.com>
2016-02-14 11:43:23 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
-#write your code here
+#write your code herel
 
-def countdown
+def countdown(int)
+    n = 10
+    while n >= 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
 end
+ "HAPPY NEW YEAR!"
+end
+
+countdown(10)
+
+def countdown_with_sleep(num)
+  number = 10
+    while number >= 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(1)
+end
+puts "HAPPY NEW YEAR!"
+end
+
+
